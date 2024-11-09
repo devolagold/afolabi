632f4b87f1b8ef9a0c715f4edfcb9eaab8e9fbb6
dev_olagold <olagoldlanre@gmail.com>
2024-11-09 06:00:30 +0100

    Commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}",
-  "./**/*.{html,js}"],
+    "./**/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
