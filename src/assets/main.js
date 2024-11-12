3c55f4e27c321c58866ff9e5b4a2400c93010c29
dev_olagold <olagoldlanre@gmail.com>
2024-11-12 23:26:38 +0100

    Push

--- src/assets/main.js
+++ src/assets/main.js
@@ -41,3 +41,15 @@ button1.addEventListener("mouseenter", () => {
     gsap.to(button1, { boxShadow: "0 0 5px rgba(0, 0, 0, 0)", duration: 1.6 });
   });
 
+
+  const buton = document.querySelector(".buton");
+  const buttonText = document.querySelector(".buton .text");
+
+  buton.addEventListener("mouseenter", () => {
+    gsap.to(buttonText, { x: -5, duration: 0.2, ease: "power2.out" });
+  });
+  
+  buton.addEventListener("mouseleave", () => {
+    gsap.to(buttonText, { x: 0, duration: 0.2, ease: "power2.out" });
+  });
+  
